7d878b808c6654da7e6c7bb86519f51ffe28f560
freejoe76 <jmurphy@denverpost.com>
2015-08-13 10:31:51 -0400

    deploy the live game too

--- deploy.bash
+++ deploy.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 source source.bash
 python misery.py
+python misery.py --name live-20150813 --live
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
 ./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
