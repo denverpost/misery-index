f026c5765640b49d9ca43c1cd334ccfc897e7229
freejoe76 <joe.murphy@gmail.com>
2015-08-13 12:27:57 -0600

    Update deploy script

--- deploy.bash
+++ deploy.bash
@@ -4,3 +4,6 @@ python misery.py
 python misery.py --name live-20150813 --live
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
 ./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
+sleep 20
+python misery.py --name live-20150813 --live
+./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
