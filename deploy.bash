4ac3e4232beecf981e15de5e52e81dc24a121ae1
freejoe76 <jmurphy@denverpost.com>
2016-04-12 19:40:25 -0400

    Get the template ready ***BOILERPLATE***

--- deploy.bash
+++ deploy.bash
@@ -3,7 +3,7 @@ source source.bash
 python misery.py --name 2016
 #python misery.py --name live-20150813 --live
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
-./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
-sleep 20
+#./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
+#sleep 20
 #python misery.py --name live-20150813 --live
-./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
+#./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
