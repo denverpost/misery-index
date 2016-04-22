30f47a8f511c098ef1eeb0aae84da696d3f23daf
freejoe76 <jmurphy@denverpost.com>
2016-04-22 15:46:20 -0400

    Update deploy for 2016

--- deploy.bash
+++ deploy.bash
@@ -3,6 +3,9 @@ source source.bash
 python misery.py --name 2016
 #python misery.py --name live-20150813 --live
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
+curl -X PURGE http://extras.denverpost.com/app/misery-index/
+curl -X PURGE http://extras.denverpost.com/app/misery-index/output/2016.json
+curl -X PURGE http://extras.denverpost.com/app/misery-index/output/scores.json
 #./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
 #sleep 20
 #python misery.py --name live-20150813 --live
